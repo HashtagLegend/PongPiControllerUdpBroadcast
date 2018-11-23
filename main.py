4406865c5f86ef2b6ab4f535f61fb70eb5ec01f8
Patrick Thomas Ørum <patr421b@edu.easj.dk>
2018-11-23 11:16:34 +0100

    trying broadcast

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from sense_hat import SenseHat
 
 sense = SenseHat()
+sense.clear
 
 
 BROADCAST_TO_PORT = 7250
@@ -21,7 +22,7 @@ while True:
 	z=round(z, 0)
 	data = ("x={0}, y={1}, z={2}".format(x, y, z))
 	s.sendto(bytes(data,"UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
-	time.sleep(2)
+	time.sleep()
 
   
   
