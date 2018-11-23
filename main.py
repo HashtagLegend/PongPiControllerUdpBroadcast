0452ba9320377a757b5abe6576113a822fda8899
Patrick Thomas Ørum <patr421b@edu.easj.dk>
2018-11-23 10:49:05 +0100

    tried ned accel

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ s.setsockopt(SOL_SOCKET,SO_BROADCAST, 1)
 
 
 
-#while True:
+while True:
     s.sendto(bytes(data,"UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
   	acceleration = sense.get_accelerometer_raw()
 	  x = acceleration['x']
