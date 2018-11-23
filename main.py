0148a524a8bfef4932793c7d90a4c780d8f65ff9
Patrick Thomas Ørum <patr421b@edu.easj.dk>
2018-11-23 11:26:49 +0100

    changed to acceleromer from _raw

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ s = socket(AF_INET, SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET,SO_BROADCAST, 1)
 
 while True:
-	acceleration = sense.get_accelerometer_raw()
+	acceleration = sense.get_accelerometer()
+	
 	x = acceleration['x']
 	y = acceleration['y']
 	z = acceleration['z']
