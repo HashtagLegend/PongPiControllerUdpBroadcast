7086592ec0c71be78efea2305438326ba9032169
Patrick Thomas Ørum <patr421b@edu.easj.dk>
2018-11-23 10:14:04 +0100

    update datetime

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ s = socket(AF_INET, SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET,SO_BROADCAST, 1)
 
 while True:
-  data = "Current time" + str(datatime.now)
+  data = "Current time" + str(datatime.now())
   s.sendto(bytes(data,"UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
   print(data)
   time.sleep(4)
